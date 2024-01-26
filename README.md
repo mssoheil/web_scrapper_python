87a928e47b5fd90c67f52e291b56bbdb2881b5ee
mssoehil <mssoheil88@yahoo.com>
2024-01-26 11:23:52 -0500

    feat: implement flat crawler

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# web_scrapper_python
+# simple web scarapper
+#### it will crawl the page and find the links then it will recursively check each links to find the working links
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from requests import get
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-def getPage(url:str):
+def getAllLinks(url:str, keyword: str):
     res = get(url)
+
+    uniqueUrls = set()
     
     soup = BeautifulSoup(res.content, "html")
     
@@ -10,8 +13,37 @@ def getPage(url:str):
     tags = soup.find_all("a")
 
     for item in tags:
-        print(item.get("href"))
-    return list
+        href = item.get("href")
+        if keyword in href:
+
+            if href not in uniqueUrls:
+                uniqueUrls.add(href)
+                normalizedUrl = urljoin(url, href)
+
+                list.append(normalizedUrl)
 
 
-print(getPage(input("what is the url? ")))
+    return uniqueUrls
+
+def checkUrls(urlList: set, keyword: str):
+
+    good = open("good_urls.txt", "w+")
+    bad = open("bad_urls.txt", "w+")
+
+    for url in urlList:
+        try:
+            res = get(url)
+            if res.status_code == 200:
+                good.write(url + "\n")
+        except:
+            bad.write(url + "\n")
+
+    pass
+
+targetUrl = input("what is the url? ")
+keyword = input("what is the keyword? ")
+
+links = getAllLinks(targetUrl, keyword)
+
+checkUrls(links, keyword)
+
