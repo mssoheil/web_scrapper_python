9fce630d0d49cc1e46c16d1f87af7b7f390d20bb
mssoehil <mssoheil88@yahoo.com>
2024-01-27 12:16:37 -0500

    feat: implement recursive checking

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def getAllLinks(url:str, keyword: str):
 
     for item in tags:
         href = item.get("href")
+        if href == None:
+            continue
+
         if keyword in href:
 
             if href not in uniqueUrls:
@@ -25,25 +28,52 @@ def getAllLinks(url:str, keyword: str):
 
     return uniqueUrls
 
-def checkUrls(urlList: set, keyword: str):
-
-    good = open("good_urls.txt", "w+")
-    bad = open("bad_urls.txt", "w+")
+def checkUrls(urlList: list, keyword: str, goodList: set, badList: set):
 
     for url in urlList:
         try:
             res = get(url)
             if res.status_code == 200:
-                good.write(url + "\n")
-        except:
-            bad.write(url + "\n")
+                if url not in goodList:
+                    print(f"checking URL {url}")
+                    goodList.add(url)
+                    links = getAllLinks(url, keyword)
+                    sublists = checkUrls(links, keyword, goodList, badList)
 
-    pass
+                    goodList.update(sublists["good"])
+                    badList.update(sublists["bad"])
+        except Exception as e:
+            print(f"Error checking URL {url}: {e}")
+            badList.add(url)
+
+    return {"good": goodList, "bad": badList}
+
+def writeToFile(goodUrls: set, badUrls: set):
+    good = open("good_urls.txt", "w+")
+    bad = open("bad_urls.txt", "w+")
+
+    for url in goodUrls:
+        good.write(url + "\n")
+        
+
+    for url in badUrls:
+        bad.write(url + "\n")
+        
 
 targetUrl = input("what is the url? ")
 keyword = input("what is the keyword? ")
 
+goodList = set()
+badList = set()
+
 links = getAllLinks(targetUrl, keyword)
 
-checkUrls(links, keyword)
+urlList = checkUrls(links, keyword, goodList, badList)
+
+print("good -----", urlList["good"])
+print("bad -----", urlList["bad"])
+
+writeToFile(urlList["good"], urlList["bad"])
+
+
 
